071b581c91427c4b1ecb7cf13c7ffdad63203654
Vijayalakshmi R <vijayalakshmi022006_bit27@mepcoeng.ac.in>
2026-02-04 10:14:17 +0530

    Add files via upload

---
+++ cycle.c.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+   bool hasCycle(struct ListNode *head) {
+   struct ListNode *slow = head, *fast = head;
+   if(head==NULL||head->next==NULL)
+   {
+      return false;
+   }
+   while(slow && fast && fast->next)
+   {
+    slow=slow->next;
+    fast=fast->next->next;
+    if(slow==fast)
+    {
+        return true;
+    }
+
+    
+   }
+    return false;
+}
---
+++ minin rotate.c.c
@@ -0,0 +1,16 @@
+int findMin(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++)
+    {
+        for(int j=i+1;j<numsSize;j++)
+        {
+            if(nums[i]>nums[j])
+            {
+                int temp= nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp; 
+                 }              
+        }
+    }
+  
+   return nums[0];   
+}
